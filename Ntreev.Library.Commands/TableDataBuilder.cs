ea7f4d8fabca6c62aa067a5b7991bf6b13bae5bd
s2quake <s2quake@ntreev.com>
2016-10-12 19:52:33 +0900

    출력기 확장 기능 추가

---
+++ Ntreev.Library.Commands/TableDataBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ntreev.Library.Commands
+{
+    public class TableDataBuilder
+    {
+        private readonly object[] headers;
+        private readonly List<string[]> rows = new List<string[]>();
+        private string[][] data;
+
+        public TableDataBuilder(params object[] headers)
+        {
+            this.headers = headers;
+        }
+
+        public void Add(params object[] items)
+        {
+            if (this.headers != null && this.headers.Length != items.Length)
+                throw new ArgumentOutOfRangeException();
+
+            this.rows.Add(items.Select(item => item.ToString()).ToArray());
+        }
+
+        public string[][] Data
+        {
+            get
+            {
+                if (this.data == null)
+                {
+                    if (this.headers != null)
+                        this.rows.Insert(0, this.headers.Select(item => item.ToString()).ToArray());
+                    this.data = this.rows.ToArray();
+                }
+                return this.data;
+            }
+        }
+    }
+}
