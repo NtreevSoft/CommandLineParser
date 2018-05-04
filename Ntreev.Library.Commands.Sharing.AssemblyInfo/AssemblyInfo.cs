2038a1afde4158eeb2679a080382a21009345a12
s2quake <s2quake@ntreev.com>
2018-05-04 17:05:59 +0900

    3.6.18124.1704 라이선스 파일명 변경 사용하지 않는 파일 제거

--- Ntreev.Library.Commands.Sharing.AssemblyInfo/AssemblyInfo.cs
+++ Ntreev.Library.Commands.Sharing.AssemblyInfo/AssemblyInfo.cs
@@ -1,13 +1,13 @@
-﻿using System.Reflection;
+using System.Reflection;
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
 [assembly: AssemblyConfiguration("Release")]
 #endif
 [assembly: AssemblyCompany("NtreevSoft")]
-[assembly: AssemblyCopyright("Copyright © NTREEV SOFT 2018")]
+[assembly: AssemblyCopyright("Copyright © Ntreev Soft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.6.0.0")]
-[assembly: AssemblyFileVersion("3.6.18101.1808")]
-[assembly: AssemblyInformationalVersion("3.6.18101.1808")]
+[assembly: AssemblyFileVersion("3.6.18124.1704")]
+[assembly: AssemblyInformationalVersion("3.6.18124.1704")]
